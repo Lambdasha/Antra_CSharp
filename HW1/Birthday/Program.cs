3246f0cf61cc11c351b964557a9bb1dd8f724332
Lambdasha <shajingshu@gmail.com>
2025-05-06 20:07:59 -0700

    Birthday

---
+++ HW1/Birthday/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+DateTime birthDate = new DateTime(1999, 12, 20);
+DateTime today = DateTime.Today;
+
+int days = (int)(today - birthDate).TotalDays;
+int daysToNextAnniversary = 10000 - (days % 10000);
+DateTime nextAnniversary = today.AddDays(daysToNextAnniversary);
+Console.WriteLine($"Born on: {birthDate:yyyy-MM-dd}");
+Console.WriteLine($"Today is: {today:yyyy-MM-dd}");
+Console.WriteLine($"You are {days} days old.");
+Console.WriteLine($"Your next 10,000 day anniversary falls on {nextAnniversary:yyyy-MM-dd}.");
