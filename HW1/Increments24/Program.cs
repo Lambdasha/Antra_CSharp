7984327781591910d32af12edf163c3f2fbdf14f
Lambdasha <shajingshu@gmail.com>
2025-05-06 20:24:10 -0700

    Increments

---
+++ HW1/Increments24/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+for (int i = 1; i <= 4; i++)
+{
+    int a = i;
+    for (int j = 0; j < 24; j = j + a)
+    {
+        Console.Write(j);
+        Console.Write(",");
+    }
+    Console.Write(24);
+    Console.WriteLine();
+}
