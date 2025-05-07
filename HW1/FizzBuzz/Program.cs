762fd3564651214fda922fa3bbe905963c1b9595
Lambdasha <shajingshu@gmail.com>
2025-05-06 21:02:24 -0700

    fizzbuzz

---
+++ HW1/FizzBuzz/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// The max value of byte is 255.
+// int max = 500;
+// if (max > byte.MaxValue)
+// {
+//     Console.WriteLine($"Warning: max ({max}) exceeds byte.MaxValue ({byte.MaxValue}).");
+// }
+// else
+// {
+//     for (byte i = 0; i < max; i++)
+//     {
+//         Console.WriteLine(i);
+//     }
+// }
+
+for (int i = 1; i <= 100; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+        Console.WriteLine("fizzbuzz");
+    else if (i % 5 == 0)
+        Console.WriteLine("buzz");
+    else if (i % 3 == 0)
+        Console.WriteLine("fizz");
+    else Console.WriteLine(i);
+}
+
