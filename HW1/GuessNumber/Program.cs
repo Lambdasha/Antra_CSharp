65146b2d000071b58d3833fdbacd77219d77b4d9
Lambdasha <shajingshu@gmail.com>
2025-05-06 19:46:26 -0700

    Guess number

---
+++ HW1/GuessNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int guessedNumber = int.Parse(Console.ReadLine());
+
+var rng = new Random();
+int a = rng.Next(1, 3);
+if (guessedNumber < 1 || guessedNumber > 3)
+{
+    Console.WriteLine("Invalid guesses: outside of the range of numbers.");
+}
+else if (guessedNumber == a)
+{
+    Console.WriteLine("Correct guess!");
+}
+else if (guessedNumber > a)
+{
+    Console.WriteLine("Too high!");
+}
+else if (guessedNumber < a)
+{
+    Console.WriteLine("Too low!");
+}
