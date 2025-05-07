c74f5199f44618e00da12e06058a8581461f43bc
Lambdasha <shajingshu@gmail.com>
2025-05-07 13:16:03 -0700

    LongestSequence

---
+++ HW2/LongestSequence_05/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string str = Console.ReadLine();
+string[] strSplit = str.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+int[] intSplit = new int[strSplit.Length];
+for (int i = 0; i < strSplit.Length; i++)
+{
+    intSplit[i] = int.Parse(strSplit[i]);
+}
+int max = -1;
+int start = 0;
+int end = 0;
+int posStart = 0;
+
+for (int i = 0; i < intSplit.Length; i++)
+{
+    if (intSplit[i] == intSplit[posStart])
+    {
+        if (i - posStart > max)
+        {
+            max = i - posStart;
+            start = posStart;
+            end = i;
+        }
+    }
+    else
+    {
+        posStart = i;
+    }
+}
+
+for (int j = start; j <= end; j++)
+{
+    Console.Write(intSplit[j] + " ");
+}
+Console.WriteLine();
