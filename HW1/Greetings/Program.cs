3e5a5b05f6f501b634755535596c8b995ce45636
Lambdasha <shajingshu@gmail.com>
2025-05-06 20:17:20 -0700

    Greetings

---
+++ HW1/Greetings/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+DateTime now = DateTime.Now;
+int hour = now.Hour;
+if (hour >= 5 && hour < 12)
+    Console.WriteLine("Good Morning");
+if (hour >= 12 && hour < 18)
+    Console.WriteLine("Good Afternoon");
+if (hour >= 18 && hour < 21)
+    Console.WriteLine("Good Evening");
+if (hour < 5 || hour >= 21)
+    Console.WriteLine("Good Night");
