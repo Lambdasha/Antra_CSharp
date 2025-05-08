0709f37117b113d77599a3572b3663a17d439a08
Lambdasha <shajingshu@gmail.com>
2025-05-07 23:04:25 -0700

    update fibonacci

---
+++ HW3/Fibonacci/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine(Fibonacci(i));
+        }
+    }
+    public static int Fibonacci(int n)
+    {
+        if (n == 1 || n == 2)
+        {
+            return 1;
+        }
+        else
+        {
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
