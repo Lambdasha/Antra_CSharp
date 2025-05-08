e928b0257c5bdd6567cc1c7020f879c999966b72
Lambdasha <shajingshu@gmail.com>
2025-05-07 22:50:16 -0700

    Palidromes and url

---
+++ HW2/FindPalindromes/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.RegularExpressions;
+
+String str = Console.ReadLine();
+string[] tokens = Regex.Split(str, "[^A-Za-z]+");
+
+var palindromes = new List<string>();
+foreach (var token in tokens)
+{
+    string rev = new string(token.Reverse().ToArray());
+    if (token == rev && !palindromes.Contains(token))
+        palindromes.Add(token);
+}
+
+Console.WriteLine(string.Join(", ", palindromes.OrderBy(s => s)));
+
+
+
---
+++ HW2/ParseURL/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string url = Console.ReadLine();
+
+string protocol = "";
+string server   = "";
+string resource = "";
+
+int protoSep = url.IndexOf("://");
+
+string rest = url;
+if (protoSep >= 0)
+{
+    protocol = url.Substring(0, protoSep);
+    rest = url.Substring(protoSep + 3);
+}
+
+int resSep = rest.IndexOf('/');
+if (resSep >= 0)
+{
+    server = rest.Substring(0, resSep);
+    resource = rest.Substring(resSep + 1);
+}
+else
+{
+    server = rest;
+}
+
+Console.WriteLine($"[protocol] = \"{protocol}\"");
+Console.WriteLine($"[server] = \"{server}\"");
+Console.WriteLine($"[resource] = \"{resource}\"");
